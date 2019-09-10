e68096773647eea70bb0b923d12d95604689f126
wollbo <Hilding@n135-p32.eduroam.kth.se>
2019-09-10 17:00:27 +0200

    uppg6

--- uppg6.m
+++ uppg6.m
@@ -6,7 +6,7 @@ mu = [1 0.3;-1 0.3;0 -0.1];%;-2 -5];
 sigma = [0.2 0.2;0.2 0.2;0.3 0.3];%; 2 6];
 %mu = [1 2;5 7];%;-2 -5];
 %sigma = [1 0.8;0.5 2];%; 2 6];
-datapoints = 500;
+datapoints = 50;
 
 
 [data1, target1] = generateData(datapoints,[mu(1,:); mu(3,:)],[sigma(1,:); sigma(3,:)]);
@@ -14,26 +14,37 @@ datapoints = 500;
 
 data = [data1; data2];
 target = [target1; target2];
-
-nodes1 = 20;
-nodes2 = 1;
-inputs = 2;
-W = 0.001*randn(nodes1,inputs+1);
-V = 0.001*randn(nodes2,nodes1);
-eta = 0.0001;
-outputs = 1;
-alpha = 0.9;
 data1 = data1';
 data2 = data2';
 target1 = target1';
 target2 = target2';
 data = [data1 data2];
 target = [target1 target2];
-epochs = 2000;
+
+%%
+
+nodes1 = 10;
+nodes2 = 1;
+inputs = 2;
+W = 0.01*randn(nodes1,inputs+1);
+V = 0.01*randn(nodes2,nodes1);
+eta = 0.01;
+outputs = 1;
+alpha = 0.9;
+epochs = 1000;
 X = data;
 nData = length(X);
 X = [X;ones(1,nData)];
 t = target;
+
+%%
+
+dataSplit1 = data(:,target==1); % bara för viualisering
+dataSplit2 = data(:,target==-1);
+scatter(dataSplit1(1,:),dataSplit1(2,:))
+hold on
+scatter(dataSplit2(1,:),dataSplit2(2,:))
+hold off
 
 %%
 dw = zeros(size(W));
@@ -57,7 +68,18 @@ for k = 1:epochs
 
     guess(k,:) = sign(z2);
     error(k) = mean((guess(k,:)-t).^2);
+    rate(k) = 1-length(target(target==guess(k,:)))/length(target);
     % add tError as (W*data-targets)
 
 end
-plot(error)
+
+%%
+
+f1 = figure('Name', 'figures/32error')
+hold on
+plot(rate)
+ylabel('Error Rate')
+xlabel('Epoch')
+
+%%
+legend('N=2','N=5','N=10');
