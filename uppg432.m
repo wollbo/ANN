8110ff6d88735c93b63dd5b5b5ccff864dc75160
Hilding Wollbo <wollbo@kth.se>
2019-09-26 14:17:06 +0200

    mer fix på 4.2

--- uppg432.m
+++ uppg432.m
@@ -13,7 +13,7 @@ x(tau) = 1.5;
 for i = tau+1:N-1
     x(i+1) = x(i)+beta*x(i-tau)/(1+x(i-tau).^n)-gamma*x(i);
 end
-sigma = 0.03; % add noise
+sigma = 0.18; % add noise
 x = x + sigma*randn(size(x));
 
 
@@ -44,12 +44,12 @@ lambda = 10e-4; % ger inte bättre prestanda än lambda = 0 : kolla om weight deca
 
 
 alpha = 0.9;
-eta = 5*10e-4; % på gränsen, konvergerar ej för eta = 10e-3. learning rate verkar vara avgörande faktor för prestanda
+eta = 10e-4; % på gränsen, konvergerar ej för eta = 10e-3. learning rate verkar vara avgörande faktor för prestanda
 T = 20; % minimal amount of epochs
 
-%%
-
-for m = 1:10
+%% Tre lager
+eta = 3*10e-4; % på gränsen, konvergerar ej för eta = 10e-3. learning rate verkar vara avgörande faktor för prestanda
+for m = 1:100
     W1 = 0.1*rand(nodes1,inputs+1);
     W2 = 0.1*rand(nodes2,nodes1+1);
     V = 0.1*rand(nodesOut,nodes2+1);
@@ -120,8 +120,69 @@ z2 = [z2;ones(1,length(z2))];
 MSE(m) = mean(((output(test)-a3').^2));
 end
 disp('done')
-mm = mean(MSE);
-mvar = var(MSE);
+mm3 = mean(MSE)
+mvar3 = var(MSE)
+
+%% Two layer network
+eta = 0.005;
+for m = 1:100
+    W = 0.1*rand(nodes1,inputs+1);
+    V = 0.1*rand(nodesOut,nodes1+1);
+    X = [input(train,:) ones(length(input(train)),1)]';
+    t = output(train)';
+    dw = zeros(size(W));
+    dv = zeros(size(V));
+    n_epochs = 0;
+for k = 1:epochs
+    [a1,z1] = forwardGeneral(W,X); 
+    z1 = [z1;ones(1,length(z1))];
+    
+    [a2,z2] = forwardGeneral(V,z1); 
+    dY = a2;
+    
+    weights = cell(3,1);
+    weights{1} = W;
+    weights{2} = V;
+
+    delta2 = (weightDecayError(a2,t,lambda,weights)).*dY;
+    delta1 = backwardGeneral(a2,V,delta2);
+
+    dw = updateGeneral(dw,alpha,delta1,X);
+    dv = updateGeneral(dv,alpha,delta2,z1);
+
+    dw = (dw .* alpha) - (delta1 * X') .* (1-alpha);
+    dv = (dv .* alpha) - (delta2 * z1') .* (1-alpha);
+    
+    W = W + eta*dw;
+    V = V + eta*dv;
+    
+    error_train(k) = mean(((a2-t)).^2);
+    
+    [b1,u1] = forwardGeneral(W,X_valid); 
+    u1 = [u1;ones(1,length(u1))];
+    [b2,u2] = forwardGeneral(V,u1);
+    error_valid(k) = mean((b2-t_valid).^2);
+    
+    
+    
+    if ~(k > T && error_valid(k)>error_valid(k-1) && error_valid(k)>error_valid(k-2) && error_valid(k)>error_valid(k-3) && error_valid(k)>error_valid(k-4) && error_valid(k)>error_valid(k-5))
+        %break % might not find global minimum but always a low value
+        W_opt = W;
+        V_opt = V;
+        n_epochs = n_epochs + 1;
+    end
+ 
+end
+X = [input(test,:) ones(length(input(test)),1)]';
+[a1,z1] = forwardGeneral(W_opt,X); 
+z1 = [z1;ones(1,length(z1))];
+[a2,z2] = forwardGeneral(V_opt,z1);
+
+MSE(m) = mean(((output(test)-a2').^2));
+end
+disp('done')
+mm2 = mean(MSE)
+mvar2 = var(MSE)
 %%
 n_graph = min(n_epochs*5,length(error_valid));
 
